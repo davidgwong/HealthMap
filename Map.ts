a6a367541a3004d5747ed4e4eab1bd87b719607f
David Wong <11532851+davidgwong@users.noreply.github.com>
2023-11-23 17:57:05 -0800

    Add registerForShots()

--- Map.ts
+++ Map.ts
@@ -29,6 +29,15 @@ type Cities = {
   Richmond: City;
 };
 
+type BlockInfo = {
+  label: "H" | "F" | "C" | "x";
+};
+
+type mapKey = {
+  cityKey: number;
+  blockKey: number;
+};
+
 /*
   A unique personal health number (string)
   A full name (string)
@@ -70,29 +79,39 @@ class PersonClass {
           dequeue(); Remove a person from the queue
           size() Check the number of people in the queue
 */
-class ClinicClass {
-  private _name: string;
-  private _numOfStaff: number;
-  private _blockNum: number;
-  private _waitlistQueue: Person[];
+class DecoratedClinic {
+  public name: string;
+  public numOfStaff: number;
+  public blockNum: number;
+  private waitlistQueue: Person[];
 
-  constructor(name: string, numOfStaff: number, blockNum: number) {
-    this._name = name;
-    this._numOfStaff = numOfStaff;
-    this._blockNum = blockNum;
-    this._waitlistQueue = [];
+  constructor(clinic: Clinic) {
+    this.name = clinic.name;
+    this.numOfStaff = clinic.staff;
+    this.blockNum = clinic.blockNum;
+    this.waitlistQueue = [];
   }
 
-  public enqueue(person: Person) {}
+  public enqueue(person: Person) {
+    this.waitlistQueue.push(person);
+  }
 
-  public dequeue() {}
+  public dequeue() {
+    this.waitlistQueue.shift();
+  }
 
   public size() {
-    return this._waitlistQueue.length;
+    return this.waitlistQueue.length;
   }
+}
 
-  public getBlockNum() {
-    return this._blockNum;
+class DecoratedHousehold {
+  public blockNum: number;
+  public inhabitants: Person[];
+
+  constructor(household: Household) {
+    this.blockNum = household.blockNum;
+    this.inhabitants = household.inhabitants;
   }
 }
 
@@ -100,13 +119,17 @@ class HealthMap {
   private _cities: Cities;
   private _blocksInLargestCity: number;
   private _intakeAgeThreshold: number;
-  private _highLevelMap: string[][];
+  private _highLevelMap: BlockInfo[][];
+  public _HouseholdMap: Map<string, DecoratedHousehold>;
+  public _ClinicMap: Map<string, DecoratedClinic>;
 
   constructor() {
     this._cities = {} as Cities;
     this._blocksInLargestCity = 0;
     this._intakeAgeThreshold = 0;
-    this._highLevelMap= [];
+    this._highLevelMap = [];
+    this._HouseholdMap = new Map();
+    this._ClinicMap = new Map();
   }
 
   public async initializeHealthMap(filePath: string) {
@@ -134,27 +157,64 @@ class HealthMap {
     return this._intakeAgeThreshold;
   }
 
-  private updateHighLevelMap() {
-    function updateCityMap(city: City, blocksInLargestCity: number) {
-      let cityMap: string[] = Array(blocksInLargestCity).fill("x");
-      city.households.forEach((household) => {
-        let foundUnvaccinatedPerson = household.inhabitants.find(
-          (person) => person.isVaccinated == false
-        );
-        if (foundUnvaccinatedPerson) {
-          cityMap[household.blockNum] = "H";
-        } else {
-          cityMap[household.blockNum] = "F";
-        }
-      });
-      city.clinics.forEach((clinic) => {
-        cityMap[clinic.blockNum] = "C";
-      });
-      return cityMap;
-    }
-    this._highLevelMap[0] = updateCityMap(this._cities.Burnaby, this._blocksInLargestCity);
-    this._highLevelMap[1] = updateCityMap(this._cities.Vancouver, this._blocksInLargestCity);
-    this._highLevelMap[2] = updateCityMap(this._cities.Richmond, this._blocksInLargestCity);
+  public getHighLevelMap() {
+    return this._highLevelMap;
+  }
+
+  private initializeCityMap(
+    city: City,
+    cityIndex: number,
+    blocksInLargestCity: number
+  ) {
+    let cityMap: BlockInfo[] = Array(blocksInLargestCity).fill({
+      label: "x",
+    });
+    city.households.forEach((household) => {
+      let foundUnvaccinatedPerson = household.inhabitants.find(
+        (person) => person.isVaccinated == false
+      );
+      if (foundUnvaccinatedPerson) {
+        cityMap[household.blockNum] = {
+          label: "H",
+        };
+      } else {
+        cityMap[household.blockNum] = {
+          label: "F",
+        };
+      }
+      this._HouseholdMap.set(
+        cityIndex + "-" + household.blockNum,
+        new DecoratedHousehold(household)
+      );
+    });
+    city.clinics.forEach((clinic) => {
+      cityMap[clinic.blockNum] = {
+        label: "C",
+      };
+      this._ClinicMap.set(
+        cityIndex + "-" + clinic.blockNum,
+        new DecoratedClinic(clinic)
+      );
+    });
+    return cityMap;
+  }
+
+  private initializeHighLevelMap() {
+    this._highLevelMap[0] = this.initializeCityMap(
+      this._cities.Burnaby,
+      0,
+      this._blocksInLargestCity
+    );
+    this._highLevelMap[1] = this.initializeCityMap(
+      this._cities.Vancouver,
+      1,
+      this._blocksInLargestCity
+    );
+    this._highLevelMap[2] = this.initializeCityMap(
+      this._cities.Richmond,
+      2,
+      this._blocksInLargestCity
+    );
   }
 
   /* 
@@ -167,8 +227,14 @@ class HealthMap {
   The "C" symbols on the map represent Clinics.
   */
   public printMap() {
-    this.updateHighLevelMap();
-    console.log(this._highLevelMap);
+    this.initializeHighLevelMap();
+    this._highLevelMap.forEach((city) => {
+      let printout = "";
+      city.forEach((block) => {
+        printout += block.label + " ";
+      });
+      console.log(printout);
+    });
   }
 
   /*
@@ -180,7 +246,54 @@ class HealthMap {
       -- That person's isVaccinated status is set to true.
   */
   public registerForShots() {
+    function nearestClinicIndex(currBlock: number, city: BlockInfo[]) {
+      let left = 0;
+      let right = currBlock + 1;
+      if (currBlock > 0) left = currBlock - 1;
+      if (currBlock > city.length - 1) right = currBlock;
+      console.log("currBlock: " + currBlock + " left: " + left + " right: " + right);
+      while ((city[left].label !== "C") && (city[right].label !== "C")) {
+        if (left > 0) left -= 1;
+        if (right < city.length - 1) right += 1;
+        // console.log("currBlock: " + currBlock + " left: " + left + " right: " + right);
+        // console.log(city[left].label + " " + (city[right].label === "C"));
+        // if (right == 3) var input = prompt("Enter data here", "");
+      }
+      if (city[left].label == "C") return left;
+      else return right;
+    }
 
+    this._highLevelMap.forEach((city, cityIndex) => {
+      city.forEach((block, blockIndex) => {
+        if (
+          this._HouseholdMap.get(cityIndex + "-" + blockIndex) &&
+          block.label == "H"
+        ) {
+          let currHousehold = this._HouseholdMap.get(
+            cityIndex + "-" + blockIndex
+          );
+          let nearestClinic = nearestClinicIndex(blockIndex, city);
+          currHousehold!.inhabitants.forEach((inhabitant, inhabitantIndex) => {
+            if (!inhabitant.isVaccinated) {
+              if (inhabitant.age > this._intakeAgeThreshold) {
+                currHousehold!.inhabitants[inhabitantIndex].isVaccinated = true;
+                let updatedClinic = this._ClinicMap.get(
+                  cityIndex + "-" + nearestClinic
+                );
+                updatedClinic!.enqueue(
+                  currHousehold!.inhabitants[inhabitantIndex]
+                );
+                this._ClinicMap.set(
+                  cityIndex + "-" + nearestClinic,
+                  updatedClinic!
+                );
+              }
+            }
+          });
+          this._HouseholdMap.set(cityIndex + "-" + blockIndex, currHousehold!);
+        }
+      });
+    });
   }
 }
 
@@ -192,5 +305,6 @@ export {
   Household,
   City,
   Cities,
-  ClinicClass,
+  DecoratedClinic,
+  DecoratedHousehold,
 };
--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import { HealthMap } from "./Map.ts";
 
-let currentIntake = 50;
+let currentIntake = 30;
 
 async function buildHealthMap(filePath: string) {
   const map = new HealthMap();
@@ -13,7 +13,11 @@ async function main() {
   const map = await buildHealthMap("./data.json");
   map.printMap();
   console.log("---End of Map---");
-  // map.registerForShots();
+  console.log(map._HouseholdMap.get("0-0"));
+  console.log(map._ClinicMap.get("0-3"));
+  map.registerForShots();
+  console.log(map._HouseholdMap.get("0-0"));
+  console.log(map._ClinicMap.get("0-3"));
   // const report = new ReportMaker(new ComplexReport(map));
   // report.printDetails();
   // console.log("---End of Report---")
